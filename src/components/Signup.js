7daffd11b2ff92530cec86e4051e7333c1fff585
VrajRP2004 <vrajrp4@gmail.com>
2024-05-10 19:43:12 +0530

    some changes on signpage according login page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -31,15 +31,15 @@ const Signup = () => {
     <div className='container'>
       <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
+                    <label htmlFor="name" id="labelname" className="form-label">Name</label>
                     <input type="text" className="form-control" value={credentials.name} onChange={onChange} id="name"  required minLength={2} name="name" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
+                    <label htmlFor="email" id="labelemail" className="form-label">Email address</label>
                     <input type="email" className="form-control"  onChange={onChange} id="email1" required minLength={2}  name="email" aria-describedby="emailHelp" value={credentials.email} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
+                    <label htmlFor="password" id="labelpassword" className="form-label">Password</label>
                     <input type="password"   onChange={onChange} className="form-control"  id="password"name="password"   required minLength={2} value={credentials.password}/>
                 </div>
 
